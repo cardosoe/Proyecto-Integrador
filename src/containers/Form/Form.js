cd7d119e1d5361eee899738857af0bb188b2d8ff
Ezequiel Cardoso <ezequielcardoso@gmail.com>
2021-09-05 20:49:05 -0300

    deleted commented lines

--- src/containers/Form/Form.js
+++ src/containers/Form/Form.js
@@ -3,35 +3,6 @@ import Globos from "../../assets/Images/contact-image.png";
 import Form from 'react-bootstrap/Form'
 import "../Form/Form.scss";
 
-//const ContactForm = () => {
-  //return (
-    //<div className="container-contacto">
-      //<div className="RectanguloGrisContacto">
-        //<Form>
-          //<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            //<Form.Label>Name</Form.Label>
-            //<Form.Control type="name" placeholder="Name" />
-          //</Form.Group>
-          //<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            //<Form.Label>Email address</Form.Label>
-            //<Form.Control type="email" placeholder="name@example.com" />
-           //</Form.Group>
-          //<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            //<Form.Label>Phone Number</Form.Label>
-            //<Form.Control type="tel" placeholder="Phone" />
-           //</Form.Group>
-          //<Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            //<Form.Label>Example textarea</Form.Label>
-            //<Form.Control as="textarea" rows={3} />
-          //</Form.Group>
-        //</Form>
-      //</div>
-    //</div>
-  //);
-//};
-
-//export default ContactForm;
-
 const ContactForm = () => {
   return (
     <div className="container-contacto">
