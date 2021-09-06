8e923cf44872c163acb71c104c76c417c99d9e06
Ezequiel Cardoso <ezequielcardoso@gmail.com>
2021-09-06 12:00:00 -0300

    app promoto to prodction on heroku

--- src/axios/index.js
+++ src/axios/index.js
@@ -3,13 +3,15 @@ import axios from "axios"
 
 const instance = axios.create({
   // baseURL: 'http://joseamietta-laravel-api.herokuapp.com/api/'
-  baseURL: 'http://localhost:8000/api/about/save'
+  // baseURL: 'http://localhost:8000/api/about/save'
+  baseURL: 'https://laravel-25whatts.herokuapp.com/api/about/save'
+
 })
 
 const sendEmail = async (data) => {
   const response = await instance({
     method: 'post',
-    url: 'http://localhost:8000/api/about/save',
+    url: 'https://laravel-25whatts.herokuapp.com/api/about/save',
     data: data
   });
 
