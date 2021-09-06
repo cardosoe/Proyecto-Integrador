96d4eacc3b632e3b2f95e77e14408af6ddc5af01
Ezequiel Cardoso <ezequielcardoso@gmail.com>
2021-09-05 21:18:37 -0300

    Axios installed and coded - form functionality coded

--- src/containers/Form/Form.js
+++ src/containers/Form/Form.js
@@ -1,9 +1,66 @@
-import React from "react";
+import React, {useState} from "react";
 import Globos from "../../assets/Images/contact-image.png";
-import Form from 'react-bootstrap/Form'
+import {Form, Alert} from 'react-bootstrap'
 import "../Form/Form.scss";
+import { sendEmail } from "../../axios"
 
 const ContactForm = () => {
+
+const [validated, setValidated] = useState(false);
+  const [formData, setFormData] = useState({})
+
+  const [showAlert, setShowAlert] = useState(false)
+  const [error, setError] = useState(false)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+
+    setValidated(true);
+
+    if (form.checkValidity() === false) {
+      e.stopPropagation();
+    } else {
+      let response
+      
+      try {
+        response = await sendEmail(formData);
+
+        if (response.status === 201) {
+          setShowAlert(true)
+          setError(false);
+
+          form.reset()
+
+          setValidated(false);
+
+          setTimeout(()=>{setShowAlert(false)}, 3000)
+        } else {
+          setError(true)
+        }
+      } catch(err) {
+        setShowAlert(true)
+        setError(true)
+      }
+    }
+  }
+
+  const handleOnChange = (e) => {
+    // Vamos a crear nuestro objeto formData
+    // {
+    //   name: 'Franco'
+    //   email: "franco@gmail.com"
+    //   phone: 346787666656
+    //   message: 'sadasdasdasd asd asd '
+    // }
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
   return (
     <div className="container-contacto">
       <div className="RectanguloGrisContacto">
@@ -37,8 +94,13 @@ const ContactForm = () => {
           className="FotoGlobos"
         />
       </div>
+        { showAlert 
+          ? error 
+            ? <Alert variant={'danger'}>Hubo un error al enviar el email </Alert> 
+            : <Alert variant={'success'}>Email enviado con éxito</Alert> 
+          : null }
     </div>
   );
 };
 
-export default ContactForm;
+  export default ContactForm;
